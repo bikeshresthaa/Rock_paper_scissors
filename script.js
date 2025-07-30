ad0592965ac70f9f273199dad65f7c122af37c2e
bikeshresthaa <eshbikesh@gmail.com>
2025-07-30 12:05:21 +0545

    create function to get human choice

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+// function for computer choice
 function getComputerChoice(){
     const options = ["rock", "scissors", "paper"];
     return options[Math.floor(Math.random() * options.length)];
 }
 
 
-computerchoice = getComputerChoice();
-console.log(computerchoice);
+
+
+//function to get human input
+function getHumanChoice(){
+    let input = prompt("Enter your choice: ", "");
+    let normalizedInput = input.toLowerCase();
+    console.log(`you selected ${normalizedInput}`);
+
+}
+
+getHumanChoice();
